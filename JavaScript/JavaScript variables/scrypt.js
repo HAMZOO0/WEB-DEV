fef0432c977b606c055461d72d82f41bba7fde49
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-03-05 08:17:33 -0800

    Add files via upload

---
+++ JavaScript/JavaScript variables/scrypt.js
@@ -0,0 +1,21 @@
+
+var a = 6;
+var b = 100;
+var c = "gg";
+console.log(a + b + 100)
+
+// her we create  a variables 
+
+console.log(typeof a, typeof b, typeof c)
+// type of use to find datatype 
+
+// LET - ? local scope 
+// VAR - ? global  scope 
+
+{
+    let a = 4;
+    console.log(a)
+
+}
+
+console.log(a)
