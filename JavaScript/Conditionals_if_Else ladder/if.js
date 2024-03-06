d0e999446e01d6e5840881ffefd29a0f05855bc5
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-03-06 09:22:22 -0800

    Add files via upload

---
+++ JavaScript/Conditionals_if_Else ladder/if.js
@@ -0,0 +1,17 @@
+let age = 20
+let sub = 6;
+
+if (age > 18) {
+    console.log("You can Drive")
+
+}
+// if ((age-sub)>18) {
+//     console.log("You can Drive")
+
+// }
+
+else
+    console.log("You can't drive")
+
+// semicolom is not nessory 
+
---
+++ JavaScript/Operators/===.js
@@ -0,0 +1,11 @@
+console.log(3==3)
+// 3==3 compare values 
+
+console.log("3"==3)
+// again compare a value 
+
+console.log("3"===3)
+//  now we use [===] to compare value and datatype also 
+
+
+console.log("3"!==3)
