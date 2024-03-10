57ec53b7c573aceb51b3bcdf47b3fe5c025790f4
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-03-10 08:43:58 -0700

    Add files via upload

---
+++ JavaScript/Functions/Functions.js
@@ -0,0 +1,33 @@
+function Greeting(name) {
+    console.log("Hi " + name)
+}
+
+Greeting("Hamza")
+
+/////////////////////////////////////////////////////
+
+function sum1(a,b) {
+    console.log(a+b)
+    
+}
+
+sum1(1,2)
+
+//////////////////////////////////////////////////////
+
+function sum(a,b) {
+    return a+b;
+
+}
+
+result=sum(8,2)
+
+console.log(result)
+
+//////////////////////////////////////////////////////
+// Arrow Function 
+
+fun = (x)=> console.log("Here is ::",x);
+    
+fun(1)
+fun(2)
---
+++ JavaScript/Functions/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+
+function sum(a,b) {
+    console.log(a+b)
+    
+}
+
+sum(1,2)
+
+//////////////////////////////////////////////////////
+
+function sum(a,b) {
+    return a+b;
+
+}
+
+result=sum(8,2)
+
+console.log(result)
