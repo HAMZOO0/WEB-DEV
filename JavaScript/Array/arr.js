d2c34ca2b36fbb22633a18a8636a2accc0469bbb
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-03-18 11:39:09 -0700

    Add files via upload

---
+++ JavaScript/Array/arr.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 45]
+
+console.log(arr, typeof arr)
+
+console.log(arr.length)
+
+// here how we convert array into string 
+console.log(arr.toString())
+
+
+// convert comma into "xyz"
+console.log(arr.join(" ::: "))
+
+
+// remove last element 
+arr.pop()
+console.log(arr)
---
+++ JavaScript/Array/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,45]
+
+console.log(arr,typeof arr)
+
+console.log(arr.length)
+
+// here how we convert array into string 
+console.log(arr.toString())
